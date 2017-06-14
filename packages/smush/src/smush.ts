bddd9a8caccb872b01124ef24aadec1731689558
mikepham <contact@mikepham.com>
2017-06-13 22:36:21 -0400

    added string method

--- packages/smush/src/smush.ts
+++ packages/smush/src/smush.ts
@@ -1,32 +1,36 @@
 import { merge } from 'lodash'
+
 import * as debug from 'debug'
-import * as Promise from 'bluebird'
 import * as fs from 'fs'
+import * as Promise from 'bluebird'
 
 export class Smush {
-  private configurations: any = {}
-  private debug: debug.IDebugger = debug('Smush')
+  private root: any = {}
+  private debug: debug.IDebugger = debug('nofrills:smush')
   private reader = Promise.promisify(fs.readFile)
   private writer = Promise.promisify(fs.writeFile)
 
   clear(key: string): void {
-    delete this.configurations[key]
+    delete this.root[key]
   }
 
   json(key: string, filename: string, transform?: (object: any) => any): Promise<Smush> {
     return this.schema<any>(key, filename, transform ? transform : object => object)
+      .then(() => this)
+  }
+
+  string(key: string, value: string, transform?: (object: any) => any): Promise<Smush> {
+    return this.transform<any>(key, JSON.parse(value), transform ? transform : object => object)
+      .then(() => this)
   }
 
   schema<T>(key: string, filename: string, transform?: (object: T) => T): Promise<Smush> {
-    const self: Smush = this
     const config: any = this.get(key)
 
     return this.reader(filename)
-      .then((buffer: Buffer) => JSON.parse(buffer.toString()))
-      .then((object: T) => transform ? transform(object) : object)
-      .then((object: T) => self.set<T>(key, object))
-      .then((object: T) => self.debug(object))
-      .then(() => self)
+      .then((buffer: Buffer) => JSON.parse(buffer.toString('utf-8')))
+      .then((object: T) => this.transform<T>(key, object, transform))
+      .then(() => this)
   }
 
   get<T>(key: string): T {
@@ -42,24 +46,31 @@ export class Smush {
   toObject(key?: string): any {
     let exported: any = {}
 
-    Object.keys(this.configurations)
-      .forEach(key => exported[key] = this.configurations[key])
+    Object.keys(this.root)
+      .forEach(key => exported[key] = this.root[key])
 
     return key ? this.config<any>(key) : exported
+  }
+
+  private transform<T>(key: string, object: T, transform?: (object: T) => T): Promise<T> {
+    return Promise.resolve(object)
+      .tap((object: T) => transform ? transform(object) : object)
+      .tap((object: T) => this.set<T>(key, object))
+      .tap((object: T) => this.debug(object))
   }
 
   private config<T>(key: string, value?: T): T {
     const parts: string[] = key.split('.')
 
     if (parts.length === 1 && value) {
-      return (this.configurations[key] = value)
+      return (this.root[key] = value)
     }
 
     return this.path(parts)
   }
 
   private path(parts: string[]): any {
-    let current: any = this.configurations
+    let current: any = this.root
     parts.forEach(part => current = current[part] || {})
     return current
   }
