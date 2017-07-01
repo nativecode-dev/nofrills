6fa009ce134709fcccab072625f59eee0d6a7871
Mike Pham <support@nativecode.com>
2017-07-01 19:42:32 -0400

    reworking all of this

--- packages/types/src/declarations/zipcodes-regex.d.ts
+++
@@ -1,8 +0,0 @@
-declare interface ZipCodesRegex {
-  [key: string]: string
-}
-
-declare module 'zipcodes-regex' {
-  const zipcodes: ZipCodesRegex
-  export = zipcodes
-}
--- packages/types/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './TypeDefinition'
-export * from './TypeDefinitions'
