48ec48518b76ddde9b746580a19938e510fb8e78
Mike Pham <support@nativecode.com>
2017-07-02 12:41:21 -0400

    fixing slice call

--- packages/lincoln/src/Interceptors/Debug.ts
+++ packages/lincoln/src/Interceptors/Debug.ts
@@ -5,7 +5,7 @@ import { Interceptor, Log } from '../Lincoln'
 export const Debug: Interceptor = (log: Log): Log => {
   if (log.parameters.length && typeof log.parameters[0] === 'string') {
     const message: string = log.parameters[0]
-    const args: any[] = log.parameters.slice(0)
+    const args: any[] = log.parameters.slice(1)
     debug(log.namespace)(message, ...args)
   } else {
     debug(log.namespace)(log.parameters)
