7178cc266766b06256e26951c7ed86af5c99b5d8
Mike Pham <support@nativecode.com>
2017-07-02 09:38:19 -0400

    fixing declaration

--- packages/lincoln/src/index.ts
+++ packages/lincoln/src/index.ts
@@ -1,8 +1,2 @@
 export * from './Lincoln'
-
-import * as interceptors from './Interceptors'
-
-export const Interceptors: any = {
-  Console: interceptors.Console,
-  Debug: interceptors.Debug,
-}
+export * from './Interceptors'
