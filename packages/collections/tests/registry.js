201a6bf88f663bbab98c89ef046ad009e1244417
Mike Pham <support@nativecode.com>
2017-07-03 18:58:58 -0400

    fixing coverage

--- packages/collections/tests/registry.js
+++ packages/collections/tests/registry.js
@@ -5,9 +5,15 @@ const Registry = require('../lib').Registry
 describe('when using Registry', () => {
   const KEY = 'key'
   const VALUE = '<value>'
+
   it('should add and resolve item', () => {
     const registry = new Registry()
     registry.register(KEY, VALUE)
     expect(registry.resolve(KEY)).to.equal(VALUE)
+  })
+
+  it('should return undefined when no key found', () => {
+    const registry = new Registry()
+    expect(registry.resolve(KEY)).to.be.undefined
   })
 })
