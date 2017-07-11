8e36c88a4620dd20db7925afa8adbafe2c6b7cae
Mike Pham <support@nativecode.com>
2017-07-10 21:05:07 -0400

    limiting requirement for collections

--- packages/lincoln/src/Options.ts
+++ packages/lincoln/src/Options.ts
@@ -1,9 +1,9 @@
 import { Registry } from '@nofrills/collections'
-import { Filter, Interceptor } from './Types'
+import { Filter, Interceptor, LincolnRegistry } from './Types'
 
 export interface Options {
-  filters: Registry<Filter>,
-  interceptors: Registry<Interceptor>,
+  filters: LincolnRegistry<Filter>,
+  interceptors: LincolnRegistry<Interceptor>,
   namespace: string
   separator: string
 }
--- packages/lincoln/src/Types.ts
+++ packages/lincoln/src/Types.ts
@@ -1,4 +1,7 @@
+import { Registry } from '@nofrills/collections'
 import { Log } from './Log'
 
 export type Filter = (log: Log) => boolean
 export type Interceptor = (log: Log) => Log
+
+export class LincolnRegistry<T extends Filter | Interceptor> extends Registry<T> { }
