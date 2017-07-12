8b1713030bc6b0333b484c25dcf16e9c0e752995
Mike Pham <support@nativecode.com>
2017-07-11 21:53:45 -0400

    fixing build

--- packages/smush/src/Logging.ts
+++ packages/smush/src/Logging.ts
@@ -1,14 +1,4 @@
-import * as logging from '@nofrills/lincoln'
+import { Lincoln } from '@nofrills/lincoln-debug'
 import * as scrubs from '@nofrills/scrubs'
 
-const scrubber: logging.Interceptor = (log: logging.Log) => {
-  log.parameters = scrubs.scrub(log.parameters)
-  return log
-}
-
-const options: logging.Options = {
-  interceptors: [scrubber, logging.Debug],
-  namespace: 'nativecode:smush'
-}
-
-export const Logger: logging.Lincoln = new logging.Lincoln(options)
+export const Logger: Lincoln = new Lincoln('nativecode:smush')
