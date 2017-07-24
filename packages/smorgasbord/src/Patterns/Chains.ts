b1caa9dfbde7389f07bbdf40fa8db00de20c0315
Mike Pham <support@nativecode.com>
2017-07-24 19:17:56 -0400

    fixing issue with Partial of Partial >.>

--- packages/smorgasbord/src/Patterns/Chains.ts
+++ packages/smorgasbord/src/Patterns/Chains.ts
@@ -1,6 +1,6 @@
 import { Lincoln, Logger } from '../Logging'
 
-export type ChainHandler<T, R> = (object: T, next: ChainHandlerLink<T, Partial<R>>) => Partial<R>
+export type ChainHandler<T, R> = (object: T, next: ChainHandlerLink<T, R>) => Partial<R>
 export type ChainHandlerLink<T, R> = (object: T) => Partial<R>
 export type ChainHandlers<T, R> = Array<ChainHandler<T, R>>
 
@@ -22,7 +22,7 @@ export class Chain<T, R> {
     const initial = (obj: T, n: ChainHandler<T, R>): Partial<R> => result
     const handlers = reverse ? this.handlers.reverse() : this.handlers
     const proxy = handlers.reduce((previous: ChainHandler<T, R>, current: ChainHandler<T, R>): ChainHandler<T, R> => {
-      return (obj: T, next: ChainHandlerLink<T, Partial<R>>): Partial<R> => {
+      return (obj: T, next: ChainHandlerLink<T, R>): Partial<R> => {
         return current(object, (o: T): Partial<R> => previous(obj, next))
       }
     }, initial)
--- packages/smorgasbord/tests/chains.ts
+++ packages/smorgasbord/tests/chains.ts
@@ -4,45 +4,47 @@ import { Chain, ChainHandler, ChainHandlerLink } from '../src'
 // tslint:disable:no-unused-expression
 
 describe('when using Chain of Responsibilty pattern', () => {
+  interface Response {
+    text: string
+  }
+
   it('should call all registered handlers', () => {
-    const first: ChainHandler<any, any> = (object: any, next: ChainHandlerLink<any, any>): any => {
-      const response: any = next(object)
-      response.first = true
-      expect(response.second).to.be.undefined
+    const first: ChainHandler<any, Response> = (object: any, next: ChainHandlerLink<any, Response>): Partial<Response> => {
+      const response: Partial<Response> = next(object)
+      expect(response.text).to.be.undefined
+      response.text = 'first'
       return response
     }
 
-    const second: ChainHandler<any, any> = (object: any, next: ChainHandlerLink<any, any>): any => {
-      const response: any = next(object)
-      response.second = true
-      expect(response.first).to.be.true
+    const second: ChainHandler<any, Response> = (object: any, next: ChainHandlerLink<any, Response>): Partial<Response> => {
+      const response: Partial<Response> = next(object)
+      expect(response.text).to.be.equal('first')
+      response.text = 'second'
       return response
     }
 
-    const chain: Chain<any, any> = new Chain<any, any>()
-    const result: any = chain.add(first).add(second).execute({})
-    expect(result.first).to.be.true
-    expect(result.second).to.be.true
+    const chain: Chain<any, Response> = new Chain<any, Response>()
+    const result: Partial<Response> = chain.add(first).add(second).execute({})
+    expect(result.text).to.be.equal('second')
   })
 
   it('should call all registered handlers in reverse', () => {
-    const first: ChainHandler<any, any> = (object: any, next: ChainHandlerLink<any, any>): any => {
-      const response: any = next(object)
-      response.first = true
-      expect(response.second).to.be.true
+    const first: ChainHandler<any, Response> = (object: any, next: ChainHandlerLink<any, Response>): Partial<Response> => {
+      const response: Partial<Response> = next(object)
+      expect(response.text).to.be.equal('second')
+      response.text = 'first'
       return response
     }
 
-    const second: ChainHandler<any, any> = (object: any, next: ChainHandlerLink<any, any>): any => {
-      const response: any = next(object)
-      response.second = true
-      expect(response.first).to.be.undefined
+    const second: ChainHandler<any, Response> = (object: any, next: ChainHandlerLink<any, Response>): Partial<Response> => {
+      const response: Partial<Response> = next(object)
+      expect(response.text).to.be.undefined
+      response.text = 'second'
       return response
     }
 
-    const chain: Chain<any, any> = new Chain<any, any>()
-    const result: any = chain.add(first).add(second).execute({}, true)
-    expect(result.first).to.be.true
-    expect(result.second).to.be.true
+    const chain: Chain<any, Response> = new Chain<any, Response>()
+    const result: Partial<Response> = chain.add(first).add(second).execute({}, true)
+    expect(result.text).to.be.equal('first')
   })
 })
