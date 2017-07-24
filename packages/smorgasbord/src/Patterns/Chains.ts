a71badf9f3b37504115250da18c949122525ce06
Mike Pham <support@nativecode.com>
2017-07-24 18:31:40 -0400

    fixing chains

--- packages/smorgasbord/src/Patterns/Chains.ts
+++ packages/smorgasbord/src/Patterns/Chains.ts
@@ -1,11 +1,15 @@
+import { Lincoln, Logger } from '../Logging'
+
 export type ChainHandler<T, R> = (object: T, next: ChainHandlerLink<T, Partial<R>>) => Partial<R>
 export type ChainHandlerLink<T, R> = (object: T) => Partial<R>
 export type ChainHandlers<T, R> = Array<ChainHandler<T, R>>
 
 export class Chain<T, R> {
-  private handlers: ChainHandlers<T, R>
+  private readonly handlers: ChainHandlers<T, R>
+  private readonly log: Lincoln
   constructor(handlers?: ChainHandlers<T, R>) {
     this.handlers = handlers ? handlers : []
+    this.log = Logger.extend('chain')
   }
 
   public add(handler: ChainHandler<T, R>): Chain<T, R> {
@@ -15,10 +19,13 @@ export class Chain<T, R> {
 
   public execute(object: T, reverse?: boolean, initializer?: () => Partial<R>): R {
     const result: Partial<R> = initializer ? initializer() : {}
-    const reducer = (previous: Partial<R>, current: ChainHandler<T, R>): Partial<R> => {
-      return Object.assign(previous, current(object, (obj: T): Partial<R> => previous))
-    }
-    return (reverse ? this.handlers.reverse() : this.handlers)
-      .reduce(reducer, result) as R
+    const initial = (obj: T, n: ChainHandler<T, R>): Partial<R> => result
+    const handlers = reverse ? this.handlers.reverse() : this.handlers
+    const fence = handlers.reduce((previous: ChainHandler<T, R>, current: ChainHandler<T, R>): ChainHandler<T, R> => {
+      return (obj: T, next: ChainHandlerLink<T, Partial<R>>): Partial<R> => {
+        return current(object, (o: T): Partial<R> => previous(obj, next))
+      }
+    }, initial)
+    return fence(object, (o: T): Partial<R> => result) as R
   }
 }
