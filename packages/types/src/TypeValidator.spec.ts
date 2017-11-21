a5ef30ffb22579890e23813deebe35c39c5a69d2
Mike Pham <support@nativecode.com>
2017-11-20 22:48:44 -0500

    cleanup

--- packages/types/src/TypeValidator.spec.ts
+++ packages/types/src/TypeValidator.spec.ts
@@ -32,9 +32,8 @@ describe('when using types library for validation', () => {
       expect(validate('string', 'any')).to.equal(true)
     })
 
-    it('should validate value is an array', () => {
-      expect(validate([1, 2, 3], 'array')).to.equal(true)
-    })
+    it('should validate value is an array', () => expect(validate([1, 2, 3], 'array')).to.equal(true))
+
     it('should validate value is NOT an array', () => {
       expect(validate(Date.now(), 'array')).to.equal(false)
       expect(validate('123', 'array')).to.equal(false)
@@ -45,42 +44,39 @@ describe('when using types library for validation', () => {
       expect(validate(false, 'boolean')).to.equal(true)
       expect(validate(false, 'boolean')).to.equal(true)
     })
+
     it('should validate value is NOT a boolean', () => {
       expect(validate(Date.now(), 'boolean')).to.equal(false)
       expect(validate('123', 'boolean')).to.equal(false)
       expect(validate(123, 'boolean')).to.equal(false)
     })
 
-    it('should validate value is a date', () => {
-      expect(validate(new Date(), 'date')).to.equal(true)
-    })
+    it('should validate value is a date', () => expect(validate(new Date(), 'date')).to.equal(true))
+
     it('should validate value is NOT a date', () => {
       expect(validate(Date.now(), 'date')).to.equal(false)
       expect(validate('123', 'date')).to.equal(false)
       expect(validate([1, 2, 3], 'date')).to.equal(false)
     })
 
-    it('should validate value is an error', () => {
-      expect(validate(new Error(), 'error')).to.equal(true)
-    })
+    it('should validate value is an error', () => expect(validate(new Error(), 'error')).to.equal(true))
+
     it('should validate value is NOT an error', () => {
       expect(validate(new Date(), 'error')).to.equal(false)
       expect(validate('123', 'error')).to.equal(false)
       expect(validate(123, 'error')).to.equal(false)
     })
 
-    it('should validate value is a number', () => {
-      expect(validate(123, 'number')).to.equal(true)
-    })
+    it('should validate value is a number', () => expect(validate(123, 'number')).to.equal(true))
+
     it('should validate value is NOT a number', () => {
       expect(validate(new Date(), 'number')).to.equal(false)
       expect(validate('123', 'number')).to.equal(false)
       expect(validate([1, 2, 3], 'number')).to.equal(false)
     })
 
-    it('should validate value is an object', () => {
-      expect(validate({}, 'object')).to.equal(true)
-    })
+    it('should validate value is an object', () => expect(validate({}, 'object')).to.equal(true))
+
     it('should validate value is NOT an object', () => {
       expect(validate(new Date(), 'object')).to.equal(false)
       expect(validate(false, 'object')).to.equal(false)
@@ -94,23 +90,22 @@ describe('when using types library for validation', () => {
       expect(validate('6666666969', 'phone')).to.equal(true)
       expect(validate(6666666969, 'phone')).to.equal(true)
     })
+
     it('should validate value is NOT a phone', () => {
       expect(validate(false, 'phone')).to.equal(false)
       expect(validate('string', 'phone')).to.equal(false)
     })
 
-    it('should validate value is a string', () => {
-      expect(validate('string', 'string')).to.equal(true)
-    })
+    it('should validate value is a string', () => expect(validate('string', 'string')).to.equal(true))
+
     it('should validate value is NOT a string', () => {
       expect(validate(Date.now(), 'string')).to.equal(false)
       expect(validate([1, 2, 3], 'string')).to.equal(false)
       expect(validate(false, 'string')).to.equal(false)
     })
 
-    it('should validate value is a timestamp', () => {
-      expect(validate(Date.now(), 'timestamp')).to.equal(true)
-    })
+    it('should validate value is a timestamp', () => expect(validate(Date.now(), 'timestamp')).to.equal(true))
+
     it('should validate value is NOT a timestamp', () => {
       expect(validate(new Date(), 'timestamp')).to.equal(false)
       expect(validate([1, 2, 3], 'timestamp')).to.equal(false)
@@ -119,18 +114,17 @@ describe('when using types library for validation', () => {
   })
 
   describe('to validate custom type values', () => {
-    it('should validate value is an email', () => {
-      expect(validate('nobody@nowhere.com', 'email')).to.equal(true)
-    })
+
+    it('should validate value is an email', () => expect(validate('nobody@nowhere.com', 'email')).to.equal(true))
+
     it('should validate value is NOT an email', () => {
       expect(validate(Date.now(), 'email')).to.equal(false)
       expect(validate('@nowhere.com', 'email')).to.equal(false)
       expect(validate(false, 'email')).to.equal(false)
     })
 
-    it('should validate value is a postal code', () => {
-      expect(validate('12345-1234', 'postalcode')).to.equal(true)
-    })
+    it('should validate value is a postal code', () => expect(validate('12345-1234', 'postalcode')).to.equal(true))
+
     it('should validate value is NOT a postal code', () => {
       expect(validate(new Date(), 'postalcode')).to.equal(false)
       expect(validate('12345?abasddks', 'postalcode')).to.equal(false)
@@ -142,9 +136,8 @@ describe('when using types library for validation', () => {
       expect(validate('aaaaaa', 'test')).to.equal(false)
     })
 
-    it('should validate string value length is required', () => {
-      expect(validate(null, 'test')).to.equal(false)
-    })
+    it('should validate string value length is required', () => expect(validate(null, 'test')).to.equal(false))
+
   })
 
 })
