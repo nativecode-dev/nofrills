b04207c18b9d40d11ba4489c925f39bde16a4969
Mike Pham <support@nativecode.com>
2017-11-23 23:04:07 -0500

    travis

--- push.sh
+++ push.sh
@@ -4,11 +4,11 @@ RELEASE=${RELEASE:="prerelease"}
 TRAVIS_BRANCH=${TRAVIS_BRANCH:="invalid"}
 
 if [ $TRAVIS_BRANCH = "master" ] && [ $TRAVIS_EVENT_TYPE = "push" ]; then
-  RELEASE=major
+  RELEASE=minor
+  lerna publish --allow-branch "master" --cd-version $RELEASE --message "$TRAVIS_BRANCH:$RELEASE:%s"
 fi
 
 if [ $TRAVIS_BRANCH  = "master-lts" ] && [ $TRAVIS_EVENT_TYPE = "push" ]; then
-  RELEASE=minor
+  RELEASE=patch
+  lerna publish --allow-branch "master-lts" --cd-version $RELEASE --message "$TRAVIS_BRANCH:$RELEASE:%s"
 fi
-
-lerna publish --cd-version $RELEASE --message "$TRAVIS_BRANCH:$RELEASE:%s"
