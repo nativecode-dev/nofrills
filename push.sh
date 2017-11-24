c21ee1b0848b39476359af419b81325a6bfb4b46
Mike Pham <support@nativecode.com>
2017-11-23 21:03:40 -0500

    fixing publish

--- push.sh
+++ push.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-. .env.sh
+export RELEASE=${RELEASE:="prerelease"}
+export TRAVIS_BRANCH=${TRAVIS_BRANCH:="invalid"}
+
+if [[ ${TRAVIS_BRANCH:="invalid"} = "master" && ${TRAVIS_EVENT_TYPE:="push"} = "push" ]]; then
+  export RELEASE=major
+fi
+
+if [[ ${TRAVIS_BRANCH:="invalid"} = "master-lts" && ${TRAVIS_EVENT_TYPE:="push"} = "push" ]]; then
+  export RELEASE=minor
+fi
 
 lerna publish --cd-version $RELEASE --message "$TRAVIS_BRANCH:$RELEASE:%s"
