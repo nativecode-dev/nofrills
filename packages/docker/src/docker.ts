a94cdd0acb9952e86453b3cfc479c6b0b9e14eb0
Mike Pham <contact@nativecode.com>
2018-05-05 22:43:31 -0400

    docker

---
+++ packages/docker/src/docker.ts
@@ -0,0 +1,20 @@
+import * as yargs from 'yargs'
+
+const args = yargs.usage('$0 [options] <command>')
+  .options({
+    't': {
+      alias: 'tag',
+      describe: 'image tag',
+      type: 'string',
+    }
+  })
+  .command(
+    'build [context]',
+    'build the container image',
+    a => a.options({
+      't': {
+        alias: 'test'
+      }
+    }))
+  .help()
+  .argv
