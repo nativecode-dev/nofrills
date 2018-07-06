4d665a5a2d2f3d0371425320394338394ca48313
Mike Pham <contact@nativecode.com>
2018-07-06 19:19:43 -0400

    fixing push script again

--- push.sh
+++ push.sh
@@ -13,7 +13,9 @@ if [ $TRAVIS_BRANCH = "master-lts" ] && [ $TRAVIS_EVENT_TYPE = "push" ]; then
 fi
 
 if [ $TRAVIS_BRANCH = "develop" ] && [ $TRAVIS_EVENT_TYPE = "push" ]; then
-  RELEASE=prelease
+  RELEASE=prerelease
 fi
+
+echo "RELEASE=$RELEASE, BRANCH=$TRAVIS_BRANCH, EVENT=$TRAVIS_EVENT_TYPE"
 
 lerna publish --allow-branch $TRAVIS_BRANCH --cd-version $RELEASE --message "$TRAVIS_BRANCH:$RELEASE:%s" --yes
