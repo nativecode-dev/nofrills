8b1cad5023834485c082af95a20e94eab73cb3c1
Mike Pham <contact@nativecode.com>
2018-07-07 23:25:31 -0400

    fix(scrubs): fixing lodash removal

--- packages/scrubs/src/ScrubsInterceptor.ts
+++ packages/scrubs/src/ScrubsInterceptor.ts
@@ -1,8 +1,6 @@
 import { Interceptor, Log } from '@nofrills/lincoln'
 import { scrub } from './Registry'
 
-import merge = require('lodash.merge')
-
 export const ScrubsInterceptor: Interceptor = (log: Log): Log => {
-  return scrub(merge({}, log))
+  return scrub(Object.assign({}, log))
 }
