8d81a65f9933b79f640f1c722cbe2fae40aa3956
Mike Pham <contact@nativecode.com>
2018-07-22 03:00:16 -0400

    adding couchbase importer

--- typings/couchbase/src/CouchbaseImportReader.ts
+++ typings/couchbase/src/CouchbaseImportReader.ts
@@ -1,15 +1,31 @@
-import * as cheerio from 'cheerio'
+import 'isomorphic-fetch'
+
+import { load } from 'cheerio'
 
 import { URL } from 'url'
 import { Class, ImportReader } from '@nofrills/typings'
 
 export class CouchbaseImportReader extends ImportReader {
-  constructor(protected readonly url: URL) {
-    super(url)
+  constructor(version: string = '2.1.4') {
+    super(new URL(`http://docs.couchbase.com/sdk-api/couchbase-node-client-${version}/classes.list.html`))
   }
 
-  classes(): Promise<Class[]> {
-    cheerio.load()
+  async classes(): Promise<Class[]> {
+    const response = await fetch(this.url.toString())
+    const html = await response.text()
+    const $ = load(html)
+
+    const classes = this.getClassIndex($)
+    classes.map((_, element) => element.nodeValue)
+
     return Promise.resolve([])
+  }
+
+  protected getClassIndex($: CheerioStatic): Cheerio {
+    return $('dt a', $('section article dl')[0])
+  }
+
+  protected getEventIndex($: CheerioStatic): Cheerio {
+    return $('dt a', $('section article dl')[1])
   }
 }
