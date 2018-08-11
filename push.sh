4493905165777be0a74aa7e35db65c88f7020a65
Mike Pham <contact@nativecode.com>
2018-08-10 21:19:01 -0400

    travis

--- push.sh
+++ push.sh
@@ -3,20 +3,30 @@
 export RELEASE=${RELEASE:="prerelease"}
 
 TRAVIS_BRANCH=${TRAVIS_BRANCH:="invalid"}
+TAG=latest
 
 if [ $TRAVIS_BRANCH = "master" ] && [ $TRAVIS_EVENT_TYPE = "push" ]; then
   RELEASE=patch
-  lerna publish --allow-branch $TRAVIS_BRANCH --cd-version $RELEASE --message "$TRAVIS_BRANCH:$RELEASE:%s" --yes --npm-tag latest
 fi
 
 if [ $TRAVIS_BRANCH = "master-lts" ] && [ $TRAVIS_EVENT_TYPE = "push" ]; then
   RELEASE=minor
-  lerna publish --allow-branch $TRAVIS_BRANCH --cd-version $RELEASE --message "$TRAVIS_BRANCH:$RELEASE:%s" --yes --npm-tag lts
+  TAG=lts
 fi
 
 if [ $TRAVIS_BRANCH = "develop" ] && [ $TRAVIS_EVENT_TYPE = "push" ]; then
   RELEASE=prerelease
-  lerna publish --allow-branch $TRAVIS_BRANCH --cd-version $RELEASE --message "$TRAVIS_BRANCH:$RELEASE:%s" --yes --npm-tag next
+  TAG=next
 fi
 
-echo "RELEASE=$RELEASE, BRANCH=$TRAVIS_BRANCH, EVENT=$TRAVIS_EVENT_TYPE"
+MESSAGE="$TRAVIS_BRANCH:$RELEASE:%s"
+echo "RELEASE=$RELEASE, BRANCH=$TRAVIS_BRANCH, EVENT=$TRAVIS_EVENT_TYPE", TAG=$TAG
+echo "$MESSAGE"
+
+lerna publish \
+  --allow-branch $TRAVIS_BRANCH \
+  --cd-version $RELEASE \
+  --message $MESSAGE \
+  --npm-tag $TAG \
+  --yes \
+;
