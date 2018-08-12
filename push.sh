1a4552f42f9761560841c5024ac3bf6f783542f8
Mike Pham <contact@nativecode.com>
2018-08-12 13:57:03 -0400

    travis

--- push.sh
+++ push.sh
@@ -2,6 +2,7 @@
 
 export RELEASE=${RELEASE:="prerelease"}
 
+RELEASE=invalid
 TRAVIS_BRANCH=${TRAVIS_BRANCH:="invalid"}
 TAG=latest
 
@@ -19,16 +20,18 @@ if [ $TRAVIS_BRANCH = "develop" ] && [ $TRAVIS_EVENT_TYPE = "push" ]; then
   TAG=next
 fi
 
-MESSAGE="$TRAVIS_BRANCH:$RELEASE:%s"
-echo "RELEASE=$RELEASE, BRANCH=$TRAVIS_BRANCH, EVENT=$TRAVIS_EVENT_TYPE", TAG=$TAG
-echo "$MESSAGE"
+if [ ! $RELEASE = "invalid" ]; then
+  MESSAGE="$TRAVIS_BRANCH:$RELEASE:%s"
+  echo "RELEASE=$RELEASE, BRANCH=$TRAVIS_BRANCH, EVENT=$TRAVIS_EVENT_TYPE", TAG=$TAG
+  echo "$MESSAGE"
 
-lerna publish bump $RELEASE \
-  --allow-branch $TRAVIS_BRANCH \
-  --message $MESSAGE \
-  --npm-client yarn \
-  --npm-tag $TAG \
-  --yes \
-  --verify-access \
-  --verify-registry \
-;
+  lerna publish bump $RELEASE \
+    --allow-branch $TRAVIS_BRANCH \
+    --message $MESSAGE \
+    --npm-client npm \
+    --npm-tag $TAG \
+    --yes \
+    --verify-access \
+    --verify-registry \
+  ;
+fi
