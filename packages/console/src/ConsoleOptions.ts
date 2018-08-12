cf3a01c2f1aa26faf3a3343755e6809033fb980f
Mike Pham <contact@nativecode.com>
2018-08-12 14:26:31 -0400

    adding onlyWhen filter

--- packages/console/src/ConsoleOptions.ts
+++ packages/console/src/ConsoleOptions.ts
@@ -1,7 +1,9 @@
 import { IConsole } from './IConsole'
 
+export type OnlyWhen = (...args: string[]) => boolean
 export type Startup = (console: IConsole) => Promise<void>
 
 export interface ConsoleOptions {
   initializer?: Startup
+  when?: OnlyWhen
 }
