861f935f7c09385134285c7e63888a63b40e2f9b
Native Open Source <opensource+nofrills@nativecode.com>
2018-08-19 21:32:33 -0400

    feat(smush): removing bluebird use for standard promises
    affects: @nofrills/smush
    
    BREAKING CHANGE:
    removal of Bluebird dependency

--- packages/smush/src/Smush.ts
+++ packages/smush/src/Smush.ts
@@ -1,29 +1,28 @@
 import * as merge from 'deepmerge'
-import * as Promise from 'bluebird'
 import * as shortid from 'shortid'
 
+import { fs } from '@nofrills/fs'
 import { Lincoln } from '@nofrills/lincoln'
-import { FileSystem as fs } from '@nofrills/fs'
 
 import { Logger } from './Logging'
 import { SmushError } from './SmushError'
 
 export class Smush {
-  public readonly identifier: string = shortid.generate()
+  readonly identifier: string = shortid.generate()
 
   private log: Lincoln
   private root: any = {}
 
   constructor() {
-    this.log = Logger.extend('Smush')
+    this.log = Logger.extend('smush')
   }
 
-  public json(key: string, filename: string, transform?: (object: any) => any): Promise<Smush> {
+  json(key: string, filename: string, transform?: (object: any) => any): Promise<Smush> {
     return this.schema<any>(key, filename, transform)
       .then(() => this)
   }
 
-  public string(key: string, value: string, transform?: (object: any) => any): Promise<Smush> {
+  string(key: string, value: string, transform?: (object: any) => any): Promise<Smush> {
     try {
       const callback = transform ? transform : (obj: any) => obj
       const json: string = JSON.parse(value)
@@ -35,29 +34,27 @@ export class Smush {
     }
   }
 
-  public schema<T>(key: string, filename: string, transform?: (object: T) => T): Promise<Smush> {
+  schema<T>(key: string, filename: string, transform?: (object: T) => T): Promise<Smush> {
     const transformer = transform ? transform : (obj: T) => obj
 
-    return Promise.resolve(
-      fs.readFile(filename)
-        .then((buffer: Buffer) => JSON.parse(buffer.toString('utf-8')))
-        .then((object: T) => this.transform<T>(key, object, transformer))
-        .catch((error: Error) => { throw new SmushError(error) })
-        .then(() => this)
-    )
+    return fs.readFile(filename)
+      .then((buffer: Buffer) => JSON.parse(buffer.toString('utf-8')))
+      .then((object: T) => this.transform<T>(key, object, transformer))
+      .catch((error: Error) => { throw new SmushError(error) })
+      .then(() => this)
   }
 
-  public get<T>(key: string): T {
+  get<T>(key: string): T {
     return this.config<T>(key)
   }
 
-  public set<T>(key: string, ...values: T[]): T {
+  set<T>(key: string, ...values: T[]): T {
     const config: any = this.get(key)
     const merged: any = merge.all([config, ...values])
     return this.config<T>(key, merged)
   }
 
-  public toObject(key?: string): any {
+  toObject(key?: string): any {
     const exported: any = {}
 
     Object.keys(this.root)
@@ -66,11 +63,12 @@ export class Smush {
     return key ? this.config<any>(key) : exported
   }
 
-  private transform<T>(key: string, object: T, transform: (object: T) => T): Promise<T> {
-    return Promise.resolve(object)
-      .tap((obj: T) => transform(obj))
-      .tap((obj: T) => this.set<T>(key, obj))
-      .tap((obj: T) => this.log.info(obj))
+  private async transform<T>(key: string, object: T, transform: (object: T) => T): Promise<T> {
+    const transformed = transform(object)
+    this.set<T>(key, transformed)
+    this.log.debug(transformed)
+
+    return transformed
   }
 
   private config<T>(key: string, value?: T): T {
