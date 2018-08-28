e7db03a34ba48ec35df45f9241ba8ae9cdec8996
Mike Pham <mike.pham@proplogix.com>
2018-08-28 14:09:23 -0400

    fix(scrubs): adding default scrubbers and exposing and alternate option
    affects: @nofrills/scrubs

--- packages/scrubs/src/index.ts
+++ packages/scrubs/src/index.ts
@@ -1,11 +1,5 @@
+export * from './Scrubbers/index'
+
 export * from './Registry'
 export * from './Scrubs'
 export * from './ScrubsInterceptor'
-
-import { Registry } from './Registry'
-import { ArrayScrubber, KeyValueScrubber, ObjectScrubber, UrlScrubber } from './Scrubbers'
-
-Registry.register<any[]>('array', ArrayScrubber)
-Registry.register<any>('object', ObjectScrubber)
-Registry.register<string>('string', KeyValueScrubber)
-Registry.register<string>('string', UrlScrubber)
