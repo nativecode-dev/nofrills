55e6fd3b5839e2b7c50e8057739f081200a99faa
Native Open Source <opensource+nofrills@nativecode.com>
2018-09-15 02:50:47 -0400

    fix(tasks): logging
    affects: @nofrills/tasks

--- packages/tasks/src/Logging.ts
+++ packages/tasks/src/Logging.ts
@@ -2,7 +2,7 @@ export { Lincoln } from '@nofrills/lincoln-debug'
 
 import chalk from 'chalk'
 import { ScrubsInterceptor } from '@nofrills/scrubs'
-import { CreateLogger, CreateOptions, Lincoln, Log, Options, LogMessageType } from '@nofrills/lincoln-debug'
+import { CreateLogger, CreateOptions, Lincoln, Log, Options } from '@nofrills/lincoln-debug'
 
 const LoggerOptions: Options = CreateOptions('nofrills:tasks')
 LoggerOptions.interceptors.register('scrubs', ScrubsInterceptor)
@@ -10,28 +10,28 @@ export const Logger: Lincoln = CreateLogger(LoggerOptions)
 
 function colorize(log: Log): string[] {
   switch (log.type) {
-    case LogMessageType.debug:
+    case 'debug':
       return [
         chalk.dim.blue(log.namespace),
         chalk.bold.gray(...log.parameters.slice(0, 1)),
         chalk.dim.gray(...log.parameters.slice(1)),
       ]
-    case LogMessageType.error:
-    case LogMessageType.fatal:
+    case 'error':
+    case 'fatal':
       return [
         chalk.dim.blue(log.namespace),
         chalk.red(...log.parameters.slice(0, 1)),
         chalk.bgRed.white(...log.parameters.slice(1)),
       ]
-    case LogMessageType.info:
+    case 'info':
       return [
         chalk.dim.blue(log.namespace),
         chalk.dim.yellow(...log.parameters.slice(0, 1)),
         chalk.dim.white(...log.parameters.slice(1)),
       ]
-    case LogMessageType.trace:
+    case 'trace':
       return [chalk.dim.blue(log.namespace), chalk.bold.gray(...log.parameters)]
-    case LogMessageType.warn:
+    case 'warn':
       return [chalk.dim.blue(log.namespace), chalk.bold.yellow(...log.parameters)]
     default:
       return [chalk.dim.blue(log.namespace), chalk.bold.white(...log.parameters)]
