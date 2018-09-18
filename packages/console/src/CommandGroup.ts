d30bce6b2d0081c30c8d9b395b42bdd3650054b7
Mike Pham <mike.pham@proplogix.com>
2018-09-17 21:29:11 -0400

    feat(console): adding command groups
    affects: @nofrills/console

---
+++ packages/console/src/CommandGroup.ts
@@ -0,0 +1,25 @@
+import { Command, CommandResult } from './Command'
+
+export interface CommandExec {
+  (command: Command): Promise<CommandResult>
+}
+
+export class CommandGroup {
+  protected constructor(private readonly commands: Command[]) {}
+
+  static from(...commands: Command[]): CommandGroup {
+    return new CommandGroup(commands)
+  }
+
+  parallel(exec: CommandExec = cmd => cmd.exec()): Promise<CommandResult[]> {
+    return Promise.all(this.commands.map(command => exec(command)))
+  }
+
+  serial(exec: CommandExec = cmd => cmd.exec()): Promise<CommandResult[]> {
+    return this.commands.reduce(async (previous: Promise<CommandResult[]>, command: Command) => {
+      const results = await previous
+      const result = await exec(command)
+      return results.concat(result)
+    }, Promise.resolve<CommandResult[]>([]))
+  }
+}
